8a3e45143e57958c7d1b025e9a2ded65643cd2e8
julienroyd <jul.roy1311@gmail.com>
2024-01-29 11:57:25 -0500

    feat: added matrix mode

--- graph_neural_nets/random_walk_positional_encoding.py
+++ graph_neural_nets/random_walk_positional_encoding.py
@@ -14,11 +14,11 @@ A = np.array([[0, 1, 1, 0, 0, 0],
               [0, 0, 0, 1, 0, 1],
               [0, 0, 0, 1, 1, 0]])
 
-MATRIX_MODE = False
+MATRIX_MODE = True
 
-if MATRIX_MODE:
+if not MATRIX_MODE:
     # Perform the random walk for each node
-    N = 1000
+    N = 10_000
     M = 10
     m_step_random_walk = np.zeros((len(nodes), len(nodes), N))
 
@@ -43,21 +43,22 @@ if MATRIX_MODE:
     m_step_random_walk = m_step_random_walk / M
     print(f"Normalized random walk matrix:\n{m_step_random_walk}")
 
-"""
-Matrix computation of the random walk
-"""
-# The degree matrix is the diagonal matrix of the sum of the adjacency matrix (number of neighbors for each node)
-D = np.diag(np.sum(A, axis=1))
+else:
+    """
+    Matrix computation of the random walk
+    """
+    # The degree matrix is the diagonal matrix of the sum of the adjacency matrix (number of neighbors for each node)
+    D = np.diag(np.sum(A, axis=1))
 
-# The transition matrix is the inverse of the degree matrix times the adjacency matrix
-# Since the degree matrix is a diagonal matrix, its diagonal is simply the inverse of the elements of the diagonal
-T = np.linalg.inv(D) @ A
-print(f"Transition matrix:\n{T}")
+    # The transition matrix is the inverse of the degree matrix times the adjacency matrix
+    # Since the degree matrix is a diagonal matrix, its diagonal is simply the inverse of the elements of the diagonal
+    T = np.linalg.inv(D) @ A
+    print(f"Transition matrix:\n{T}")
 
-# Perform the random walk for each node using power of M of the transition matrix
-M = 10
-m_step_random_walk = np.linalg.matrix_power(T, M)
-print(f"Random walk matrix after {M} steps:\n{m_step_random_walk}")
+    # Perform the random walk for each node using power of M of the transition matrix
+    M = 10
+    m_step_random_walk = np.linalg.matrix_power(T, M)
+    print(f"Random walk matrix after {M} steps:\n{m_step_random_walk}")
 
 # Plot the random walk matrix
 sns.heatmap(m_step_random_walk, annot=True, cmap="Blues")
